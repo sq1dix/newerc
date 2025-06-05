3d9ab264863459d6e04ca183d160be5fc9435984
sq1dix <gafurshakirov26@gmail.com>
2025-06-05 13:35:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Файл: script.js
+
+function updateClock() {
+  const now = new Date();
+  const clock = document.getElementById("clock");
+  const date = document.getElementById("date");
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const localeDate = now.toLocaleDateString('ru-RU', options);
+
+  clock.textContent = now.toLocaleTimeString('ru-RU');
+  date.textContent = localeDate.charAt(0).toUpperCase() + localeDate.slice(1);
+}
+
+setInterval(updateClock, 1000);
+updateClock();
 
